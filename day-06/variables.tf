9b6998e18139fd52063c1df9bd9a9d8d57dce2ba
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-21 00:35:25 +0530

    Create variables.tf

---
+++ day-06/variables.tf
@@ -0,0 +1,13 @@
+variable "environment" {
+    type = string
+    description = "environment value"
+    default = "staging"
+}
+
+
+variable "location" {
+  type = string
+  description = "location"
+  default = "West Europe"
+}    
+
